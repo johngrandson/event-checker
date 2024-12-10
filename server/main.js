2a2ab32fcecf7f5c76dfa27cf1439ad44034b64d
João Netto <joaonettopb@hotmail.com>
2024-12-09 21:54:25 -0300

    chore: migrate .js files to .ts and .tsx

--- server/main.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { loadInitialData } from '../infra/initial-data';
-
-Meteor.startup(async () => {
-  // DON'T CHANGE THE NEXT LINE
-  await loadInitialData();
-
-  // YOU CAN DO WHATEVER YOU WANT HERE
-});
